a6555b84e7bd4aa8a01f285ef9222ecf6e74336a
Ilya Amursky <ilya@amursky.me>
2022-06-23 15:20:32 +0200

    fix: update cookies domain

--- aws-exports.js
+++ aws-exports.js
@@ -5,18 +5,19 @@ module.exports = {
     userPoolWebClientId: "2p20jmvbidrjf5rkcd311n5sie",
     mandatorySignIn: false,
     cookieStorage: {
-      domain: ".vercel.app",
-      expires: 365,
+      domain: ".ok-cognito-amplify.vercel.app",
       path: "/",
-      sameSite: "strict",
+      expires: 365,
       secure: true,
     },
     oauth: {
       domain: "testing-saml.auth.us-east-1.amazoncognito.com",
+      scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
       redirectSignIn: "https://ok-cognito-amplify.vercel.app",
       redirectSignOut: "https://ok-cognito-amplify.vercel.app",
+      // redirectSignIn: "http://localhost:3000",
+      // redirectSignOut: "http://localhost:3000",
       responseType: "code",
-      scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
     },
   },
 };
