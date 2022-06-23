7ed0a51e78e842b4e73f0a26c16be6c67113720c
Ilya Amursky <ilya@amursky.me>
2022-06-23 15:10:41 +0200

    fix: add cookies config

--- aws-exports.js
+++ aws-exports.js
@@ -4,6 +4,12 @@ module.exports = {
     userPoolId: "us-east-1_Abd5yPTuk",
     userPoolWebClientId: "2p20jmvbidrjf5rkcd311n5sie",
     mandatorySignIn: false,
+    cookieStorage: {
+      domain: "ok-cognito-amplify.vercel.app",
+      path: "/",
+      expires: 365,
+      secure: true,
+    },
     oauth: {
       domain: "testing-saml.auth.us-east-1.amazoncognito.com",
       scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
