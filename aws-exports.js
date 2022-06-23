70babb482b8515b2fc9f4a0866b8d0e2706c436b
Ilya Amursky <ilya@amursky.me>
2022-06-23 15:16:16 +0200

    fix: add cookies config

--- aws-exports.js
+++ aws-exports.js
@@ -5,19 +5,18 @@ module.exports = {
     userPoolWebClientId: "2p20jmvbidrjf5rkcd311n5sie",
     mandatorySignIn: false,
     cookieStorage: {
-      domain: "ok-cognito-amplify.vercel.app",
-      path: "/",
+      domain: ".vercel.app",
       expires: 365,
+      path: "/",
+      sameSite: "strict",
       secure: true,
     },
     oauth: {
       domain: "testing-saml.auth.us-east-1.amazoncognito.com",
-      scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
       redirectSignIn: "https://ok-cognito-amplify.vercel.app",
       redirectSignOut: "https://ok-cognito-amplify.vercel.app",
-      // redirectSignIn: "http://localhost:3000",
-      // redirectSignOut: "http://localhost:3000",
       responseType: "code",
+      scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
     },
   },
 };
