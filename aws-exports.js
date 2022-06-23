05d11fde9a2262cdc50ca3c5b31090d2a3f9bccc
Ilya Amursky <ilya@amursky.me>
2022-06-23 14:03:43 +0200

    chore: deploy to vercel

---
+++ aws-exports.js
@@ -0,0 +1,17 @@
+module.exports = {
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_Abd5yPTuk",
+    userPoolWebClientId: "2p20jmvbidrjf5rkcd311n5sie",
+    mandatorySignIn: false,
+    oauth: {
+      domain: "testing-saml.auth.us-east-1.amazoncognito.com",
+      scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
+      redirectSignIn: "https://ok-cognito-amplify.vercel.app",
+      redirectSignOut: "https://ok-cognito-amplify.vercel.app",
+      // redirectSignIn: "http://localhost:3000",
+      // redirectSignOut: "http://localhost:3000",
+      responseType: "code",
+    },
+  },
+};
